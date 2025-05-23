9a38285964ecaedd5a36990a69ef4cbd5c026d20
AbdoH7 <abdo.ah1102@gmail.com>
2025-05-23 14:22:17 +0300

    Integrated swagger

--- real-estate-api/src/main.ts
+++ real-estate-api/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { connectionSource } from './config/typeorm';
 import { seed } from './seed';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   await connectionSource.initialize();
@@ -11,6 +12,18 @@ async function bootstrap() {
   }
 
   const app = await NestFactory.create(AppModule);
+
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('Real Estate API')
+    .setDescription('The Real Estate API documentation')
+    .setVersion('1.0')
+    .addTag('real-estate')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
